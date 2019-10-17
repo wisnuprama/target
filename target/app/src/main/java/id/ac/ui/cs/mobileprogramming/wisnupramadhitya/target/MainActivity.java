c4575830a49915f35b37a1c322926a04458fd9a2
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2019-10-18 01:07:31 +0700

    chore: init app target, material theme, initial dependencies (#1)

---
+++ target/app/src/main/java/id/ac/ui/cs/mobileprogramming/wisnupramadhitya/target/MainActivity.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ target/app/src/main/java/id/ac/ui/cs/mobileprogramming/wisnupramadhitya/target/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
