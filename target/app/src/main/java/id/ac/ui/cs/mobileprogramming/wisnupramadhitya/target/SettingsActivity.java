d521cf6dc085329341a7fc5a1fd29d095e184c03
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2019-11-01 13:00:58 +0700

    feat: Settings - listen to change for theme mode & auto dark mode (#33)
    * feat: preference listener - use MVP; apply theme mode and autodark mode
    
    Signed-off-by: Wisnu Pramadhitya <wisnuprama014@gmail.com>
    
    * docs: add todo comment
    
    Signed-off-by: Wisnu Pramadhitya <wisnuprama014@gmail.com>

--- target/app/src/main/java/id/ac/ui/cs/mobileprogramming/wisnupramadhitya/target/SettingsActivity.java
+++ target/app/src/main/java/id/ac/ui/cs/mobileprogramming/wisnupramadhitya/target/SettingsActivity.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
@@ -8,10 +9,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreferenceCompat;
 
+import butterknife.BindString;
+import butterknife.ButterKnife;
 import id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.data.source.repository.PreferenceRepository;
+import id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.service.ThemeModeJobService;
 import id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.util.BuildUtils;
+import id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.util.ThemeUtils;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    @BindString(R.string.theme_mode)
+    protected String mKeyThemeMode;
+
+    @BindString(R.string.auto_dark_mode)
+    protected String mKeyAutoDarkMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,40 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        ButterKnife.bind(this);
+        subscribeToPreference();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceRepository.getSharedPreferences(getApplicationContext())
+                .unregisterOnSharedPreferenceChangeListener(this::onPreferenceChange);
+    }
+
+    private void subscribeToPreference() {
+        SharedPreferences preferences = PreferenceRepository
+                .getSharedPreferences(getApplicationContext());
+        preferences.registerOnSharedPreferenceChangeListener(this::onPreferenceChange);
+    }
+
+    /**
+     * If theme mode change, call {@link ThemeUtils} to apply new mode. Sometimes it is buggy.
+     *
+     * If auto dark mode on
+     * start job to register {@link id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.broadcastreceiver.ThemeModeReceiver}
+     * in the background.
+     *
+     * TODO implement good morning in preference
+     * TODO implement backup/export button in preference
+     */
+    void onPreferenceChange(SharedPreferences sp, String key) {
+        Context context = getApplicationContext();
+        if(mKeyThemeMode.equals(key)) {
+            ThemeUtils.applyTheme(PreferenceRepository.getThemeMode(context));
+        } else if(mKeyAutoDarkMode.equals(key)) {
+            ThemeModeJobService.scheduleJob(getApplicationContext());
+        }
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -34,7 +79,7 @@ public class SettingsActivity extends AppCompatActivity {
             disabledAutoDarkModeWhenOnDarkMode();
         }
 
-        public void disabledAutoDarkModeWhenOnDarkMode() {
+        void disabledAutoDarkModeWhenOnDarkMode() {
             Context context = getContext();
             if(context != null) {
                 boolean isThemeModeDark = context.getString(R.string.theme_value_dark).equals(PreferenceRepository.getThemeMode(context));
