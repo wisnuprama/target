56431b0110c92f21e2ae9d7c366d213868ec228f
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2019-11-08 07:18:57 +0700

    refactor: fully deprecated the okrnavigator in favor view as listener
    Signed-off-by: Wisnu Pramadhitya <wisnuprama014@gmail.com>

--- target/app/src/main/java/id/ac/ui/cs/mobileprogramming/wisnupramadhitya/target/viewmodel/ObjectivesViewModel.java
+++ target/app/src/main/java/id/ac/ui/cs/mobileprogramming/wisnupramadhitya/target/viewmodel/ObjectivesViewModel.java
@@ -1,9 +1,5 @@
 package id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.viewmodel;
 
-import android.view.MenuItem;
-import android.view.View;
-
-import androidx.annotation.NonNull;
 import androidx.databinding.Observable;
 import androidx.databinding.ObservableInt;
 import androidx.lifecycle.LiveData;
@@ -12,12 +8,10 @@ import androidx.lifecycle.ViewModel;
 
 import java.util.List;
 
-import id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.R;
 import id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.data.model.ObjectiveWithKeyResults;
 import id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.data.model.Project;
 import id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.data.source.repository.ObjectiveRepository;
 import id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.data.source.repository.ProjectRepository;
-import id.ac.ui.cs.mobileprogramming.wisnupramadhitya.target.navigator.OkrNavigator;
 
 public class ObjectivesViewModel extends ViewModel {
 
